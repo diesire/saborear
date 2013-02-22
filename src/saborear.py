dace9b22ee46b781f781bf962f472c2a378a66a8
Pablo Escalada Gómez <diesire@MacBook-de-Pablo-Escalada-Gomez.local>
2013-02-22 07:04:47 +0100

    Fix format and clean code

--- src/saborear.py
+++ src/saborear.py
@@ -9,7 +9,6 @@ from bson import json_util
 #Logger config
 logging.basicConfig(format='%(levelname)s:%(message)s', level=logging.DEBUG)
 logger = logging.getLogger('saborear')
-#logger.warning('Protocol problem: %s', 'connection reset', extra=d)
 
 #Mongo config
 _testing_collection = 'rating_testing'
@@ -33,11 +32,12 @@ def index():
 # Ratings
 @route('/ratings', method='GET')
 def get_ratings():
+    "Devuelve una lista de valoraciones, de la forma {'id': '1234567890', 'uri': '/ratings/1234567890'}"
     ratings = collection.find()
     json_docs = []
     for doc in ratings:
         _normalize_object(doc)
-        json_docs.append({'id': doc['id'], 'URI': '/ratings/{}'.format(doc['id'])})
+        json_docs.append({'id': doc['id'], 'uri': '/ratings/{}'.format(doc['id'])})
     json_docs = json.dumps(json_docs)
     logger.info('json_docs: %s', json_docs)
     response.set_header('Content-Type', 'application/json')
@@ -62,10 +62,9 @@ def post_ratings():
     except ValidationError as ve:
         abort(400, str(ve))
 
-
 @route('/ratings/<rating_id>', method='GET')
 def get_rating(rating_id):
-    
+    "Devuelve un objeto rating de la forma {'id':'1234567890', 'user_id':'test', 'target_id':'la cullar', 'value':'2', 'comment':'Malo, malo'}"
     entity = collection.find_one({'_id': ObjectId(rating_id)})
     #normalize
     entity["id"] = str(entity["_id"])
@@ -145,15 +144,15 @@ def get_target(target_id):
 def _normalize_object(obj):
         "Normalize mongo object for json serialization"
         if isinstance(obj, dict):
-            if "_id" in obj: 
+            if "_id" in obj:
                 obj["id"] = str(obj["_id"])
                 del obj["_id"]
         if isinstance(obj, list):
-            for a in obj: 
+            for a in obj:
                 _normalize_object(a)
 
 #Main
-if __name__ == '__main__':        
+if __name__ == '__main__':
     # To run the server, type-in $ python server.py
-    bottle.debug(True) # display traceback 
+    bottle.debug(True) # display traceback
     run(host='localhost', port=8080, reloader=True)
